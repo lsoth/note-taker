05615b341772d31681d0606a4f6be1d225b53e2f
Mamba <84301026+lsoth@users.noreply.github.com>
2021-07-23 21:37:52 -0700

    update server

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -5,10 +5,25 @@ const app = express();
 
 const PORT = 3001;
 
+const notes = require('./db/db.json');
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//set up express
+
 app.use(express.static('public'));
 
 app.get('/', (req, res) => res.send('Take Some Notes'));
 
 app.get('/notes', (req, res) => 
   res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+//api of notes that can be added using json
+app.get('/api', (req, res) => res.json(notes));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
 );
