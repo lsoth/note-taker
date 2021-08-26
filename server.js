14783f649cb3bad7c9d224271ca9626fd363aee9
Mamba <84301026+lsoth@users.noreply.github.com>
2021-08-26 10:52:12 -0700

    update server js routes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require ('express');
 const path = require ('path');
-
+const fs = require ('fs');
+const uuid =require
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-const PORT = process.env.PORT || 3001;
 
 const notes = require('./db/db.json');
 
@@ -15,14 +16,29 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Take Some Notes'));
+//route for homepage
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
+//route to open a note
 app.get('/notes', (req, res) => 
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
 //api of notes that can be added using json
-app.get('/api', (req, res) => res.json(notes));
+app.get('/api/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '.db/note.json'))
+);
+
+//posting a saved note
+app.post('/api/notes', (req,res) => {
+  const newNote ={
+    title: req.body.title,
+    text: req.body.text,
+    id: uuid(),
+  }
+})
 
 app.listen(PORT, () =>
   console.log(`Note Taking at http://localhost:${PORT}`)
